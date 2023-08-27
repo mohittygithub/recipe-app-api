7ef72f0ec689b15c7acfe214e8acb2d5d88cad07
Mohit Tyagi <smartfriendmohit@gmail.com>
2023-08-27 18:20:56 +0530

    added tags module

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,62 @@
+"""Tests for Models"""
+
+from decimal import Decimal
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from core import models
+
+def create_user(email='user@example.com', password='User@1234'):
+    """Create and return a user"""
+    return get_user_model().objects.create_user(email,password)
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        email = "test@example.com"
+        password = "test1234"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.com", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, "test1234")
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test1234")
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser("test@example.com", "test1234")
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test if create a new recipe is successfull"""
+        user = create_user("test@example.com", "Test@1234")
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample Recipe',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample Recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
---
+++ app/recipe/views.py
@@ -0,0 +1,44 @@
+"""Views for the recipe api's"""
+
+from rest_framework import (
+    viewsets,
+    mixins
+)
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Recipe, Tag
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View to manage recipe api's"""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Get recipes for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for the request"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create new recipe"""
+        serializer.save(user=self.request.user)
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset according to the authnticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
